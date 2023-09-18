1e7b882d53ffc3abdc2446756e41fdfacd204dd1
Keith A. Lewis <kal@kalx.net>
2023-09-18 12:06:36 -0400

    sync

--- xlauto.cpp
+++ xlauto.cpp
@@ -5,15 +5,6 @@
 extern "C" int __declspec(dllexport) xlAutoOpen()
 {
 	try {
-		//xll::counted_array_view<wchar_t> cav("Local\\xll", SECTION_MAP_WRITE| SECTION_MAP_READ, 0, 1024);
-		//xll::OPER o("abc");
-		//cav.append(o.val.str);
-		//xll::OPER o2("def");
-		//cav.append(o2.val.str);
-		//int n;
-		//n = cav.size();
-		//const wchar_t* p = cav[0];
-		//p = cav[1];
 	}
 	catch (const std::exception& ex) {
 		const char* s;
